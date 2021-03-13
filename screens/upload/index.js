0e3a5e220b966bf8738aefa41114d84df9a62120
MythriJL <mythri1020@gmail.com>
2021-03-13 21:36:18 +0530

    added navigation from upload screen to questions

--- screens/upload/index.js
+++ screens/upload/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { SafeAreaView, Text, StyleSheet, Button } from 'react-native';
 import * as  DocumentPicker  from 'expo-document-picker';
 export default class Upload extends React.Component {
-    _pickDocument = async () => {
+    _pickDocument = async (navigation) => {
         let result = await DocumentPicker.getDocumentAsync({});
-        alert(result.uri);
+        // alert(result.uri);
+        navigation.navigate('Question')
         console.log(result);
     }
     render() {
+        const {navigation} =  this.props;
         return (
             <>
                 <SafeAreaView style={styles.container}>
                     <Button
                         title="Select Document"
-                        onPress={this._pickDocument}
+                        onPress={()=>this._pickDocument(navigation)}
                     />
                 </SafeAreaView>
             </>
